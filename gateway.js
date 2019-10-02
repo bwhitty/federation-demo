a424d3df373a4f77b3bb754ea3df1a63155cd9f8
Brady Whitten <whitten@adobe.com>
2019-10-02 15:18:08 -0700

    reproduction

--- gateway.js
+++ gateway.js
@@ -1,5 +1,5 @@
 const { ApolloServer } = require("apollo-server");
-const { ApolloGateway } = require("@apollo/gateway");
+const { ApolloGateway, RemoteGraphQLDataSource } = require("@apollo/gateway");
 
 const gateway = new ApolloGateway({
   serviceList: [
@@ -7,7 +7,13 @@ const gateway = new ApolloGateway({
     { name: "reviews", url: "http://localhost:4002/graphql" },
     { name: "products", url: "http://localhost:4003/graphql" },
     { name: "inventory", url: "http://localhost:4004/graphql" }
-  ]
+  ],
+
+  buildService({ name, url }) {
+    console.log(name, url);
+
+    return new RemoteGraphQLDataSource({ url });
+  }
 });
 
 (async () => {
